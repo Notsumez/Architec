e66fe1c2f6bd17ffae92a020765f6226c21a2cb7
Notsume <adahorapirola@gmail.com>
2024-03-28 11:07:48 -0300

    front end do login feito

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { LoginDireitaComponent } from '../login-direita/login-direita.component';
-import { LoginEsquerdaComponent } from '../login-esquerda/login-esquerda.component';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [LoginDireitaComponent, LoginEsquerdaComponent, RouterLink],
+  imports: [RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
