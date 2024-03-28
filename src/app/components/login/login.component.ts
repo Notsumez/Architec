6322e517aaf9d4911a41cfded33d4b3ffea5bf0b
Notsume <adahorapirola@gmail.com>
2024-03-28 11:45:36 -0300

    cadastrar criado

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { SignupComponent } from '../signup/signup.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, SignupComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  ngOnInit() {
+    // Defina o login como verdadeiro e o signup como falso por padrão
+    this.login = true;
+    this.signup = false;
+  }
+  
+  constructor(){
+
+  }
+
+  signup:boolean = true;
+  login:boolean = true;
+
+  showLogin(){
+    if(this.login == true){
+      this.login = false;
+      this.signup = true;
+    }
+  }
+
+  showSign(){
+    if(this.signup == true){
+      this.signup = false;
+      this.login = true;
+    }
+  }
 }
