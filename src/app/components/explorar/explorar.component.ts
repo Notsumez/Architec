44b6d291a4bc656a8392c1e370d808d77f86cb98
Andrey <adahorapirola@gmail.com>
2024-04-18 10:31:20 -0300

    importação header, footer e router link para explorar

--- src/app/components/explorar/explorar.component.ts
+++ src/app/components/explorar/explorar.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-explorar',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent, RouterLink],
   templateUrl: './explorar.component.html',
   styleUrl: './explorar.component.scss'
 })
