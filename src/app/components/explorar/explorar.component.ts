a7ad85641b514478d6bc8556dc1d21057e90fef1
Andrey <adahorapirola@gmail.com>
2024-04-19 10:50:59 -0300

    exibindo a data

--- src/app/components/explorar/explorar.component.ts
+++ src/app/components/explorar/explorar.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
+import { DatePipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-explorar',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, DatePipe],
   templateUrl: './explorar.component.html',
   styleUrl: './explorar.component.scss'
 })
 export class ExplorarComponent {
-
+  dataAtual = new Date;
 }
